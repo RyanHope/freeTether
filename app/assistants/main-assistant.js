0f601ae2492d57216c4cafffc620d1b9fb892c3c
Ryan Hope <rmh3093@gmail.com>
2010-12-12 20:15:16 -0500

    freeTether

---
+++ app/assistants/main-assistant.js
@@ -0,0 +1,128 @@
+function MainAssistant() {
+
+	this.cookie = new preferenceCookie();
+	this.prefs = this.cookie.get();
+	
+}
+
+MainAssistant.prototype.setup = function() {
+
+	this.controller.document.body.className = 'palm-dark';
+
+	this.mainOptions					= this.controller.get('mainOptions');
+	this.mainOptions.style.display		= '';
+	this.wifiOptions					= this.controller.get('wifiOptions');
+	this.wifiOptions.style.display		= 'none';
+	this.btOptions						= this.controller.get('btOptions');
+	this.btOptions.style.display		= 'none';
+	this.usbOptions						= this.controller.get('usbOptions');
+	this.usbOptions.style.display		= 'none';
+	
+	this.controller.setupWidget(
+		'tetherWiFi',
+		{
+  			trueLabel:  'On',
+ 			falseLabel: 'Off',
+  			fieldName:  'tetherWiFi'
+		},
+		{
+			value : this.prefs.tetherWifi,
+ 			disabled: false
+		}
+	);
+
+	this.controller.setupWidget(
+		'tetherBT',
+		{
+  			trueLabel:  'On',
+ 			falseLabel: 'Off',
+  			fieldName:  'tetherBT'
+		},
+		{
+			value : this.prefs.tetherBT,
+ 			disabled: false
+		}
+	);
+	
+	this.controller.setupWidget(
+		'tetherUSB',
+		{
+  			trueLabel:  'On',
+ 			falseLabel: 'Off',
+  			fieldName:  'tetherUSB'
+		},
+		{
+			value : this.prefs.tetherUSB,
+ 			disabled: false
+		}
+	);
+
+	this.menuModel = {
+		visible:true,
+		items: [
+			{},
+			{label:$L('Views'), 
+			toggleCmd:'mainOptions', 
+			items:[
+				{label:$L('Main'), icon:'icon-switch', command:'mainOptions'}, 
+				{label:$L('WiFi'), icon:'icon-wifi', command:'wifiOptions'},
+				{label:$L('BT'),  icon:'icon-bt', command:'btOptions'},
+				{label:$L('USB'), icon:'icon-usb', command:'usbOptions'}
+			]},
+			{},
+		]
+	};
+	this.controller.setupWidget(Mojo.Menu.commandMenu, undefined, this.menuModel);
+	 
+};
+
+MainAssistant.prototype.handleCommand = function(event) {
+	
+	if(event.type == Mojo.Event.command) {
+		
+		switch (event.command)
+		{
+			case 'mainOptions':
+				this.mainOptions.style.display = '';
+				this.wifiOptions.style.display = 'none';
+				this.btOptions.style.display = 'none';
+				this.usbOptions.style.display = 'none';
+				break;
+				
+			case 'wifiOptions':
+				this.mainOptions.style.display = 'none';
+				this.wifiOptions.style.display = '';
+				this.btOptions.style.display = 'none';
+				this.usbOptions.style.display = 'none';
+				break;
+			
+			case 'btOptions':
+				this.mainOptions.style.display = 'none';
+				this.wifiOptions.style.display = 'none';
+				this.btOptions.style.display = '';
+				this.usbOptions.style.display = 'none';
+				break;
+				
+			case 'usbOptions':
+				this.mainOptions.style.display = 'none';
+				this.wifiOptions.style.display = 'none';
+				this.btOptions.style.display = 'none';
+				this.usbOptions.style.display = '';
+				break;
+		}
+			
+	}
+	
+}
+
+MainAssistant.prototype.activate = function(event) {
+
+};
+
+MainAssistant.prototype.deactivate = function(event) {
+
+};
+
+MainAssistant.prototype.cleanup = function(event) {
+
+};
