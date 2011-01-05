dbb96964303bb3a2dba191becd136130a0ce54e5
Ryan Hope <rmh3093@gmail.com>
2011-01-05 17:15:08 -0500

    add btPrefs scene, other ui tweaks

--- app/assistants/stage-assistant.js
+++ app/assistants/stage-assistant.js
@@ -1,6 +1,5 @@
 var vers = new versionCookie();
 var prefs = new preferenceCookie();
-var service = new FreeTetherService();
 
 function StageAssistant() {
 
@@ -15,3 +14,25 @@ StageAssistant.prototype.setup = function() {
     this.controller.pushScene('main');
 
 };
+
+function objectToString(o) {
+  var parse = function(_o) {
+    var a = [], t;
+    for(var p in _o) {
+      if(_o.hasOwnProperty(p)) {
+        t = _o[p];
+        if(t && typeof t == "object") {
+          a[a.length]= p + ":{ " + arguments.callee(t).join(", ") + "}";
+        } else {
+          if(typeof t == "string"){
+            a[a.length] = [ p+ ": \"" + t.toString() + "\"" ];
+          } else {
+            a[a.length] = [ p+ ": " + t.toString()];
+          }        
+        }
+      }
+    }
+    return a;
+  }
+  return "{" + parse(o).join(", ") + "}";  
+}
